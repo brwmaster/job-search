57c59bc6e32fc707f8bb6d3fc8d88c21e25624d2
Edwin Boon <edwin.boon@eo.nl>
2023-01-12 21:31:11 +0100

    Created the MainNav component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1 class="mt-8 text-center text-6xl">JobSearch</h1>
+  <MainNav />
 </template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainNav,
+  },
+};
+</script>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="w-full text-sm">Navigation</header>
+</template>
+
+<script>
+export default {
+  name: "MainNav",
+};
+</script>
