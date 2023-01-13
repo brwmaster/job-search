adc75e2899d5bd90071b760fc822f1ae04eeae72
Edwin Boon <edwin.boon@eo.nl>
2023-01-13 20:49:35 +0100

    Add bit of styling for the navigation

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,13 @@
 <template>
-  <header class="w-full text-sm">Navigation</header>
+  <header class="w-full text-sm">
+    <div class="top-0-left-0 fixed h-16 w-full bg-white">
+      <div
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl">DevCareers</a>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
