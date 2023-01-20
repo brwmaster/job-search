187e2637348b3b3cc2ae120dd10d616e578f0050
Edwin Boon <edwin.boon@eo.nl>
2023-01-20 16:01:36 +0100

    Add test for mainNav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -5,6 +5,19 @@
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
         <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="(menuItem, index) in menuItems"
+              :key="index"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="/" class="flex h-full items-center py-2.5">
+                {{ menuItem }}
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -16,6 +29,14 @@ export default {
   data() {
     return {
       company: "DevCareers",
+      menuItems: [
+        "Teams",
+        "Locations",
+        "Life at DevCareers",
+        "How we Hire",
+        "Students",
+        "Jobs",
+      ],
     };
   },
 };
