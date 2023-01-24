fcf4a625cc0ae27c096ea90e94613f285055de5d
Edwin Boon <edwin.boon@eo.nl>
2023-01-24 11:16:30 +0100

    wrote test for login button and profile image

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
@@ -20,5 +21,29 @@ describe("MainNav", () => {
       "Students",
       "Jobs",
     ]);
+  });
+
+  describe("when the user logs in", () => {
+    it("display user profile image", async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole("img", {
+        name: /profile image/i,
+      });
+
+      expect(profileImage).not.toBeInTheDocument();
+
+      const loginButton = screen.getByRole("button", {
+        name: /sign in/i,
+      });
+
+      await userEvent.click(loginButton);
+
+      profileImage = screen.queryByRole("img", {
+        name: /profile image/i,
+      });
+
+      expect(profileImage).toBeInTheDocument();
+    });
   });
 });
