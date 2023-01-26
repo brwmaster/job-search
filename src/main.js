b087cc11f9d282208b2e201d350916c97a74057e
Edwin Boon <edwin.boon@eo.nl>
2023-01-26 09:48:41 +0100

    Added icon and hardcoded jobs matched for now

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "@/index.css";
 import App from "@/App.vue";
 
+library.add(faSearch);
+
 const app = createApp(App);
+app.component("FontAwesomeIcon", FontAwesomeIcon);
 app.mount("#app");
