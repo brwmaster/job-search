2e41dc2d350cf75ecf8a2c0d5050039e14f82f6c
Edwin Boon <edwin.boon@eo.nl>
2023-01-26 09:20:56 +0100

    Add some basic styling for the subnav

--- src/components/SubNav.vue
+++ src/components/SubNav.vue
@@ -1,4 +1,8 @@
-<template>subnav</template>
+<template>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+    <div class="flex h-full items-center px-8">subnav</div>
+  </div>
+</template>
 
 <script>
 export default {
