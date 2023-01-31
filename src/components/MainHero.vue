0d537f9344f1308325e428abc1d4075b7b7f5c33
Edwin Boon <edwin.boon@eo.nl>
2023-01-31 16:23:36 +0100

    Add grid/cols for the header

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -1,6 +1,13 @@
 <template>
   <main>
-    <section>Hero</section>
+    <section class="flex h-screen flex-col bg-yellow-500 pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-span-1 col-start-1 border border-red-500">1 col</div>
+        <div class="col-span-5 col-start-2 border border-red-500">5 col</div>
+        <div class="col-span-5 col-start-7 border border-red-500">5 col</div>
+        <div class="col-span-1 col-start-12 border border-red-500">1 col</div>
+      </div>
+    </section>
   </main>
 </template>
 
