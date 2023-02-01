bdb358431601778b4a8aaa8ff0c3a55cd357aebe
Edwin Boon <edwin.boon@eo.nl>
2023-02-01 15:39:54 +0100

    Added basic job search form and did some cleanup for the hero

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -2,10 +2,11 @@
   <main>
     <section class="flex h-screen flex-col pt-10 pb-20">
       <div class="grid grid-cols-12">
-        <div class="col-span-5 col-start-2 border border-red-500">
+        <div class="col-span-5 col-start-2">
           <the-headline />
+          <job-search-form />
         </div>
-        <div class="col-span-5 col-start-7 border border-red-500">5 col</div>
+        <div class="col-span-5 col-start-7"></div>
       </div>
     </section>
   </main>
@@ -13,10 +14,13 @@
 
 <script>
 import TheHeadline from "@/components/TheHeadline.vue";
+import JobSearchForm from "@/components/JobSearchForm.vue";
+
 export default {
   name: "MainHero",
   components: {
     TheHeadline,
+    JobSearchForm,
   },
 };
 </script>
