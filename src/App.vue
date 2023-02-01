273006867b724e5eee39ac2553ec10c39153ffcb
Edwin Boon <edwin.boon@eo.nl>
2023-02-01 20:34:53 +0100

    Refactorment for Navigation components. Moved them to a seperate folder

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-import MainNav from "@/components/MainNav.vue";
+import MainNav from "@/components/Navigation/MainNav.vue";
 import MainHero from "@/components/MainHero.vue";
 
 export default {
