99600042ccd4de6e60dc8c57f057e65ef0c3f62f
Edwin Boon <edwin.boon@eo.nl>
2023-02-08 16:22:05 +0100

    Finished testing other props location and minimumQualifications

--- tests/unit/components/JobResults/JobListing.test.js
+++ tests/unit/components/JobResults/JobListing.test.js
@@ -6,6 +6,8 @@ describe("JobListing", () => {
   const createJobProps = (jobProps = {}) => ({
     title: "Vue Developer",
     organization: "Vue Lovers",
+    locations: ["Rotterdam", "Berlin"],
+    minimumQualifications: ["10 years experience", "Typescript", "Vitest"],
     ...jobProps,
   });
 
@@ -28,15 +30,34 @@ describe("JobListing", () => {
     const jobProps = createJobProps({ title: "React Developer" });
     renderJobListing(jobProps);
 
-    const jobTitle = screen.getByText("React Developer");
-    expect(jobTitle).toBeInTheDocument();
+    expect(screen.getByText("React Developer")).toBeInTheDocument();
   });
 
   it("renders job organization", () => {
     const jobProps = createJobProps();
     renderJobListing(jobProps);
 
-    const jobOrganization = screen.getByText("Vue Lovers");
-    expect(jobOrganization).toBeInTheDocument();
+    expect(screen.getByText("Vue Lovers")).toBeInTheDocument();
+  });
+
+  it("renders job locations", () => {
+    const jobProps = createJobProps({ locations: ["Amsterdam", "Londen"] });
+
+    renderJobListing(jobProps);
+
+    expect(screen.getByText("Amsterdam")).toBeInTheDocument();
+    expect(screen.getByText("Londen")).toBeInTheDocument();
+  });
+
+  it("renders job qualifications", () => {
+    const jobProps = createJobProps({
+      minimumQualifications: ["5 years experience", "Develop", "Code"],
+    });
+
+    renderJobListing(jobProps);
+
+    expect(screen.getByText("5 years experience")).toBeInTheDocument();
+    expect(screen.getByText("Develop")).toBeInTheDocument();
+    expect(screen.getByText("Code")).toBeInTheDocument();
   });
 });
