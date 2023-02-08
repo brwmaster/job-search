d10acd0094ccb7863c1345d521015ea6cd8a5547
Edwin Boon <edwin.boon@eo.nl>
2023-02-08 10:10:40 +0100

    simplified the SubNav test by creating a function to render the component

--- tests/unit/components/Navigation/SubNav.test.js
+++ tests/unit/components/Navigation/SubNav.test.js
@@ -2,22 +2,24 @@ import { render, screen } from "@testing-library/vue";
 import SubNav from "@/components/Navigation/SubNav.vue";
 
 describe("SubNav", () => {
-  describe("when user is on jobs page", () => {
-    it("displays job count", () => {
-      const $route = {
-        name: "JobResults",
-      };
-
-      render(SubNav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-          mocks: {
-            $route,
+  const renderSubNav = (routeName) => {
+    render(SubNav, {
+      global: {
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+        mocks: {
+          $route: {
+            name: routeName,
           },
         },
-      });
+      },
+    });
+  };
+
+  describe("when user is on jobs page", () => {
+    it("displays job count", () => {
+      renderSubNav("JobResults");
 
       const jobCount = screen.getByText("1423");
       expect(jobCount).toBeInTheDocument();
@@ -26,20 +28,7 @@ describe("SubNav", () => {
 
   describe("when user is not on jobs page", () => {
     it("does not display job count", () => {
-      const $route = {
-        name: "Home",
-      };
-
-      render(SubNav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-          mocks: {
-            $route,
-          },
-        },
-      });
+      renderSubNav("Home");
 
       const jobCount = screen.queryByText("1423");
       expect(jobCount).not.toBeInTheDocument();
