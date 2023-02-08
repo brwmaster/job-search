f792091eae63ee901a611360f4b15984f73afc38
Edwin Boon <edwin.boon@eo.nl>
2023-02-08 16:52:17 +0100

    Add tests for the JobListings

---
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/vue";
+import axios from "axios";
+import JobListings from "@/components/JobResults/JobListings.vue";
+import { RouterLinkStub } from "@vue/test-utils";
+
+// mock axios
+vi.mock("axios");
+
+describe("JobListings", () => {
+  it("fetches jobs", () => {
+    // overwrite axios.get and return a promise with the data property
+    axios.get.mockResolvedValue({ data: [] });
+    render(JobListings);
+
+    expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
+  });
+
+  it("creates a job listing for every job", async () => {
+    axios.get.mockResolvedValue({
+      data: Array(15).fill({ id: "1", title: "Vue Developer" }),
+    });
+    render(JobListings, {
+      global: {
+        stubs: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+
+    const jobListings = await screen.findAllByRole("listitem");
+    expect(jobListings).toHaveLength(15);
+  });
+});
