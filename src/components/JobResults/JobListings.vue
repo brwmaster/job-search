1c1c392e4a9e81707dbbc1961434393bee54722b
Edwin Boon <edwin.boon@eo.nl>
2023-02-08 11:59:20 +0100

    Add some dummy data for a JobListing

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,5 +1,27 @@
 <template>
-  <main class="flex-auto bg-brand-gray-2 p-8">JobListings</main>
+  <main class="flex-auto bg-brand-gray-2 p-8">
+    <ol>
+      <li class="mb-7">
+        <router-link
+          to="/jobs/results/1"
+          class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
+        >
+          <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
+            <h2 class="mb-2 text-2xl">Vue Developer (Senior)</h2>
+
+            <div class="flex flex-row align-middle">
+              <div class="mr-5">
+                <span>DevCarreers</span>
+              </div>
+              <div>
+                <span>Amsterdam</span>
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </li>
+    </ol>
+  </main>
 </template>
 
 <script>
