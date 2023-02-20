0e9d1eff04625ed623b3e1dc63d5fe00ba8ff594
Edwin Boon <edwin.boon@eo.nl>
2023-02-20 21:16:59 +0100

    Added more test and updated mainNav component

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -1,15 +1,20 @@
 import { render, screen } from "@testing-library/vue";
 import { RouterLinkStub } from "@vue/test-utils";
+import { createTestingPinia } from "@pinia/testing";
 import userEvent from "@testing-library/user-event";
 import MainNav from "@/components/Navigation/MainNav.vue";
+import { useUserStore } from "@/stores/user";
 
 describe("MainNav", () => {
   const renderMainNav = () => {
+    const pinia = createTestingPinia();
+
     const $route = {
       name: "Home",
     };
     render(MainNav, {
       global: {
+        plugins: [pinia],
         stubs: {
           FontAwesomeIcon: true,
           RouterLink: RouterLinkStub,
@@ -44,6 +49,9 @@ describe("MainNav", () => {
   describe("when the user logs in", () => {
     it("display user profile image", async () => {
       renderMainNav();
+
+      const userStore = useUserStore();
+
       let profileImage = screen.queryByRole("img", {
         name: /profile image/i,
       });
@@ -54,6 +62,7 @@ describe("MainNav", () => {
         name: /sign in/i,
       });
 
+      userStore.isLoggedIn = true;
       await userEvent.click(loginButton);
 
       profileImage = screen.getByRole("img", {
