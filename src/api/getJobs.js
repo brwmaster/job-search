a93a604f7ab24fe8203cbe68555846a1f0297cd2
Edwin Boon <edwin.boon@eo.nl>
2023-02-21 11:55:38 +0100

    Add test for getting a jobs array

--- src/api/getJobs.js
+++ src/api/getJobs.js
@@ -4,7 +4,9 @@ const getJobs = async () => {
   const baseUrl = import.meta.env.VITE_APP_API_URL;
   const url = `${baseUrl}/jobs`;
 
-  await axios.get(url);
+  const result = await axios.get(url);
+
+  return result.data;
 };
 
 export default getJobs;
--- tests/unit/api/getJobs.test.js
+++ tests/unit/api/getJobs.test.js
@@ -4,8 +4,19 @@ import getJobs from "@/api/getJobs";
 vi.mock("axios");
 
 describe("getJobs", () => {
+  beforeEach(() => {
+    axios.get.mockResolvedValue({
+      data: [{ id: 1, title: "VueJS Engineer" }],
+    });
+  });
+
   it("fetches jobs that canidates can apply to", async () => {
     await getJobs();
     expect(axios.get).toHaveBeenCalledWith("http://myfakeapi.com/jobs");
+  });
+
+  it("extract jobs from response", async () => {
+    const jobs = await getJobs();
+    expect(jobs).toEqual([{ id: 1, title: "VueJS Engineer" }]);
   });
 });
